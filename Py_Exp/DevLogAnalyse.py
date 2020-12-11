ce9b9d76fbc9b99b8c249e38f8cf1d151b128c0f
ChopinNocture <chopin_7up@icloud.com>
2020-12-11 19:32:20 +0800

    Update DevLogAnalyse.py

--- Py_Exp/DevLogAnalyse.py
+++ Py_Exp/DevLogAnalyse.py
@@ -49,8 +49,14 @@ RE_WARN_ERROR_FILTER = re.compile(r"[wW]arn(ing)?|[eE]rror")
 RE_UE4_PATH_OR_CLASS = re.compile(r"((?:[\/|\\]Game|[\/|\\]Content)(?:[\/|\\][^”\'\"“:]*)+)")
 
 
-def analyse_UE4logline(line, ana_obj):
+def analyse_UE4logline(line, time):
     if RE_WARN_ERROR_FILTER.search(line):
+        ana_obj = {
+            "time": time,
+            "text": "",
+            "catagory": "",
+            "key": ""
+        }
         match = RE_UE4_LOG_CATEGORIES.search(line)
         if match:
             for iter in LOG_CATEGORIES:
@@ -62,53 +68,41 @@ def analyse_UE4logline(line, ana_obj):
                     ana_obj["paths"] = RE_UE4_PATH_OR_CLASS.findall(ana_obj["text"])
         else:
             ana_obj["text"] = line
+            ana_obj["catagory"] = "none"
+        return ana_obj
+    return None
 
 
 def analyse_teamcity_buildlog(log_text):
     lines = log_text.splitlines()
     lines = list(set(lines))
 
-    state = 0
-    log_obj = {'head': {"text": ""}, 'body': [], 'foot': {"text": ""}}
+    log_obj = {'info': {"text": ""}, 'body': []}
     for line in lines:
         match = RE_BODY_PREFIX.match(line)
-        if state == 0:
-            if match:
-                state = 1
-            else:
-                log_obj['head']['text'] = log_obj['head']['text'] + \
-                    '\r\n' + line
-
-        if state == 1:
-            if match:
-                line_log = match.groupdict()
-                line_obj = {
-                    "time": line_log["time"],
-                    "text": "",
-                    "catagory": "",
-                    "key": ""
-                }
-                analyse_UE4logline(line_log["ue4log"], line_obj)
+        if match:
+            line_log = match.groupdict()
+            line_obj = analyse_UE4logline(line_log["ue4log"], line_log["time"])
+            if line_obj:
                 log_obj["body"].append(line_obj)
-            else:
-                state = 2
-
-        if state == 2:
-            log_obj['foot']['text'] = log_obj['foot']['text'] + \
+        else:
+            log_obj['info']['text'] = log_obj['info']['text'] + \
                 '\r\n' + line
     return log_obj
 
 
 
-testlog_path = "D:/Users/feng.yan/Documents/testlog.txt"
+testlog_path = "D:/Users/feng.yan/Documents/Rox_Alpha_Daily_Build_24.log"
 with open(testlog_path, 'r', encoding='utf-8') as file:
     text = file.read()
 
-analyse_teamcity_buildlog(text)
+result = analyse_teamcity_buildlog(text)
 
 #result = RE_UE4_PATH_OR_CLASS.findall("[16:01:31] :	 [Step 1/1]   LogMaterial: Warning: D:\TeamCity\buildAgent\work\86314017ea15d70\Project_Rox\Rox_Demo_0\Content\Environment\DungePart_A\Material\Master\MM_Rooftops_Concrete.uasset: Failed to compile Material for platform PCD3D_SM5, Default Material will be used in game.")
 #result = RE_UE4_PATH_OR_CLASS.findall("[16:03:53] :	 [Step 1/1]   LogUObjectGlobals: Warning: 加载“D:/TeamCity/buildAgent/work/86314017ea15d70/Project_Rox/Rox_Demo_0/Content/Blueprints/NPC/AI/EQSQuery/TargetInRange.uasset”时未能加载“/Script/EnvironmentQueryEditor”：无法找到文件。")
-#print(result)
+#result = analyse_UE4logline("[11:47:37] :	 [Step 1/1]   LogInit: Display: LogAutomationTest: Warning: FGridFlowItem::ItemId is not initialized properly. Module:DungeonArchitectRuntime File:Public/Frameworks/GridFlow/AbstractGraph/GridFlowAbstractItem.h", "1")
+
+print(result)
 
 # lines = text.splitlines()
 # df = pd.DataFrame(lines)
