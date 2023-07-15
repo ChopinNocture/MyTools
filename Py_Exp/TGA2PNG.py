e4343b7fd79acc49244d4f42e313bcce53aafa29
ChopinNocture <chopin_7up@icloud.com>
2023-07-15 20:47:14 +0800

    Update TGA2PNG.py

--- Py_Exp/TGA2PNG.py
+++ Py_Exp/TGA2PNG.py
@@ -1,6 +1,8 @@
 import os
 import argparse
 from PIL import Image
+import numpy as np
+import imageio
 
 
 
@@ -46,5 +48,16 @@ if __name__ == '__main__':
             rgba = img.convert("RGBA")
             destfile = os.path.join(destPath, pureName + ".tga")
             rgba.save(destfile, "TGA")
-       
-        
+        elif sts[1].lower() == "hdr":
+            pureName = sts[0]
+            img = imageio.imread(fullName, format='HDR-FI')
+            # Simply clamp values to a 0-1 range for tone-mapping
+            ldr = np.clip(img, 0, 1)
+            # Color space conversion
+            ldr = ldr**(1/2.2)
+            # 0-255 remapping for bit-depth conversion
+            ldr = 255.0 * ldr
+            destfile = os.path.join(destPath, pureName + ".png")
+            imageio.imwrite(destfile, ldr)
+            #rgba.save(destfile, "PNG")
+            #imageio.imwrite(ldr_path, ldr)
