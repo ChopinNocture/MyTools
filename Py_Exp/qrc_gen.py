1ecb7b96f8b4990eb87277ba36762d050d00dd22
ChopinNocture <chopin_7up@icloud.com>
2024-11-05 03:37:33 +0800

    Update qrc_gen.py

--- Py_Exp/qrc_gen.py
+++ Py_Exp/qrc_gen.py
@@ -2,6 +2,7 @@ import qrcode
 
 # URL to be encoded
 url = "http://footballgame.w1.luyouxia.net/UESite/sharescore/"
+url = "http://footballgame.w1.luyouxia.net/UESite/shareprize/"
 
 # Create QR code
 qr = qrcode.QRCode(
@@ -18,4 +19,6 @@ img = qr.make_image(fill_color="black", back_color="white")
 
 # Save the image
 file_path = "E:/DevDocumnets/足球互动/qrcode_score.png"
+file_path = "E:/Development/Packages/qrcode_prize.png"
+
 img.save(file_path)
