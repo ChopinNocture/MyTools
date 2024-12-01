d4fa64c679dea89384658b664500f47b292650b6
ChopinNocture <chopin_7up@icloud.com>
2024-12-02 04:02:53 +0800

    Create u2b_SkipAd_Edge.py
    use edge skip ads

---
+++ Py_Exp/u2b_SkipAd_Edge.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+driver = webdriver.Edge()
+driver.get("https://www.youtube.com")
+
+# 检测并点击“跳过广告”按钮
+def skip_ad():
+    try:
+        skip_button = driver.find_element(By.CLASS_NAME, "ytp-skip-ad-button")  # YouTube 的“跳过广告”按钮的类名
+        if skip_button:
+            print("找到跳过广告按钮，点击！")
+            ActionChains(driver).move_to_element(skip_button).click().perform()            
+    except NoSuchElementException:
+        pass
+    except Exception as e:
+        pass
+        print(f"未找到按钮或出现错误: {e}")
+
+# 自动循环检测
+while True:
+    skip_ad()
+    time.sleep(0.5)
